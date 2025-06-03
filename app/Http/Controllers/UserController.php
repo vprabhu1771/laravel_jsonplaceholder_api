7f5b09711abf0feccdd10c17097d242b49e76896
Prabhu <vprabhu1771@gmail.com>
2025-06-03 18:09:30 +0530

    fetch and display users from external API
    fetch users from external API and add search filter by name

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
 class UserController extends Controller
 {
-    //
+     public function index(Request $request)
+    {
+        try {
+            $response = Http::get('https://jsonplaceholder.typicode.com/users');
+
+            if ($response->successful()) {
+                $users = $response->json();
+                $search = $request->query('search');
+                if ($search) {
+                    $users = array_filter($users, fn($user) => stripos($user['name'], $search) !== false);
+                }
+                return view('users.index', ['users' => $users, 'search' => $search]);
+            }
+
+            return view('users.index', ['error' => 'Failed to fetch users from API']);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return view('users.index', ['error' => 'An error occurred while fetching users.']);
+        }
+    }
 }
